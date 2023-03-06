783d1872d99e8288fb29e4fd4ecb8db6a03e5873
Stan <stanislav.stepanenko@borngroup.com>
2023-03-06 18:26:17 +0200

    mocks

--- mockserver.js
+++ mockserver.js
@@ -1,4 +1,4 @@
 const http = require("http");
 const mockserver = require("mockserver");
 
-http.createServer(mockserver("mocks", { verbose: true })).listen(80);
+http.createServer(mockserver("mocks", { verbose: true })).listen(9001);
