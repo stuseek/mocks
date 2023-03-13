50f9955ead9c97783fa84bf58425e29a8da910ac
Stan <stanislav.stepanenko@borngroup.com>
2023-03-13 16:30:42 +0200

    mocks

--- mockserver.js
+++ mockserver.js
@@ -1,4 +1,19 @@
 const http = require("http");
 const mockserver = require("mockserver");
 
-http.createServer(mockserver("mocks", { verbose: true })).listen(9001);
+const options = {
+    verbose: true,
+};
+
+http.createServer(function (req, res) {
+    // Extract the origin from the Referer header
+    const origin = req.headers.referer ? new URL(req.headers.referer).origin : "";
+    console.log(origin)
+
+    // Set the Access-Control-Allow-Origin header to the origin
+    res.setHeader("Access-Control-Allow-Origin", origin);
+
+    // Create the mock server with the updated options
+    mockserver("mocks", options)(req, res);
+}).listen(9000);
+
